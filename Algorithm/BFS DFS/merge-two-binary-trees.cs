608c1c09a25fb1f043293c4bf75b1c783d7db1f8
Vladislav Evtushenko <evtushenko.v@outlook.com>
2022-03-27 13:02:38 +0300

    updated

--- Algorithm/BFS DFS/merge-two-binary-trees.cs
+++ Algorithm/BFS DFS/merge-two-binary-trees.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -12,7 +14,49 @@
  * }
  */
 public class Solution {
+    
+    // always right;
     public TreeNode MergeTrees(TreeNode root1, TreeNode root2) {
+        var stack = new Stack<TreeNode[]>();
+        
+        if (root1 == null) {
+            return root2;
+        }
+        else if (root2 == null) {
+            return root1;
+        }
+        
+        stack.Push(new TreeNode[] {root1, root2});
+        
+        // for both 1st is not null
+        while (stack.Any()) {
+            var cur = stack.Pop();
+            var l = cur[0];
+            var r = cur[1];
+            
+            if (l == null || r == null) {
+                continue;
+            }
+            
+            r.val += l.val;
+            if (r.left == null) {
+                r.left = l.left;
+            }
+            else {
+                stack.Push(new TreeNode[] {l.left, r.left});
+            }
+            if (r.right == null) {
+                r.right = l.right;
+            }
+            else {
+                stack.Push(new TreeNode[] {l.right, r.right});
+            }
+        }
+            
+        return root2;
+    }
+    
+    public TreeNode MergeTrees2(TreeNode root1, TreeNode root2) {
         if (root1 == null && root2 == null) {
             return null;
         }
