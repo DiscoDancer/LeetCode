6f976b61e8a1e227ffebaa39c265a63a752b8ee7
Vladislav Evtushenko <evtushenko.v@outlook.com>
2022-04-03 14:03:02 +0300

    updated

--- Data Structure/Array/reshape-the-matrix.cs
+++ Data Structure/Array/reshape-the-matrix.cs
@@ -7,14 +7,13 @@ public class Solution {
             return mat;
         }
         
-        var res = new int[r][];
-        for (int i = 0; i < r; i++) {
-            res[i] = new int[c];
-        }
-        
+        var res = new int[r][];        
         int k = 0;
         for (int i = 0; i < X; i++) {
             for (int j = 0; j < Y; j++) {
+                if (k % c == 0) {
+                    res[k / c] = new int[c];
+                }
                 res[k / c][k % c] = mat[i][j];
                 k++;
             }
