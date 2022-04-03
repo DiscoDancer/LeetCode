7c4a47647a445e873a7bb38fd56142225eb12671
Vladislav Evtushenko <evtushenko.v@outlook.com>
2022-04-03 12:14:13 +0300

    +1

---
+++ Data Structure/Stack  Queue/valid-parentheses.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+public class Solution {
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        
+        foreach (var c in s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.Push(c);
+            }
+            else {
+                if (!stack.Any()) {
+                    return false;
+                }
+                
+                var pop = stack.Pop();
+                
+                if (c == ')' && pop != '(') {
+                    return false;
+                }
+                if (c == '}' && pop != '{') {
+                    return false;
+                }
+                if (c == ']' && pop != '[') {
+                    return false;
+                }
+            }
+        }
+        
+        return !stack.Any();
+    }
+}
