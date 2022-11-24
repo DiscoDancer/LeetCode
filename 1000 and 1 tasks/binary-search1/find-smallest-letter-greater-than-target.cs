f0acca348d174c01d0405994d79d4737170cc336
discodancer <vlad.rabot9ga@gmail.com>
2022-11-24 11:10:43 +0300

    +1

---
+++ 1000 and 1 tasks/binary-search1/find-smallest-letter-greater-than-target.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public char NextGreatestLetter(char[] letters, char target) {
+        for (int i = 0; i < letters.Length; i++) {
+            if (letters[i] - target > 0) {
+                return letters[i];
+            }
+        }
+        
+        return letters[0];
+    }
+}
