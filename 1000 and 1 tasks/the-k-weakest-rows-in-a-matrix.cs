91995103a16a3926f385bca5db1f134d70460dee
discodancer <vlad.rabot9ga@gmail.com>
2022-11-28 16:19:21 +0300

    +1

---
+++ 1000 and 1 tasks/the-k-weakest-rows-in-a-matrix.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    // если индекс выше, то сильнее при равных
+    // переводить двоичную в десятичную
+    // считать единички можно бинарным поиском
+    // можно отсорировать матрицу по строка и тогда получить индексы за О(1)
+    // матрицу представить как массив или инвертировать
+    
+    // матрица не квадратная !
+    public int[] KWeakestRows(int[][] mat, int k) {
+        var left = k;
+        var result = new List<int>();
+        
+        // нужно баннить строки
+        
+        var isBanned = new bool[mat.Length];
+        
+        // столбец
+        for (int i = 0; i < mat[0].Length; i++) {
+            // строка
+            for (int j = 0; j < mat.Length; j++) {
+                if (!isBanned[j] && mat[j][i] == 0) {
+                    result.Add(j);
+                    left--;
+                    isBanned[j] = true;
+                    
+                    if (left == 0) {
+                        return result.ToArray();
+                    }
+                }
+            }
+        }
+        
+        while (left > 0) {
+            for (int i = 0; i < mat.Length && left > 0; i++) {
+                if (!isBanned[i]) {
+                    result.Add(i);
+                    isBanned[i] = true;
+                    left--;
+                }
+            }
+        }
+        
+        return result.ToArray();
+    }
+}
