c48d065015176b51faea964eaa6cece0705aa06a
discodancer <vlad.rabot9ga@gmail.com>
2022-11-29 11:56:35 +0300

    +1

---
+++ 1000 and 1 tasks/binary-search1/sum-of-square-numbers_1.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    // массив квадратов, по которому бегаем 2мя указателями, можно бегать с бинарным поиском
+    // второй всегда может нулем
+    
+    // оптимизация со словарем
+    // out of memory
+    public bool JudgeSquareSum(int c) {
+        if (c == 0) {
+            return true;
+        }
+        
+        var squares = new List<int>();
+        var dic = new Dictionary<int, bool>();
+        
+        for (int i = 1; i <= c; i++) {
+            var square = i*i;
+            if (square == c) {
+                return true;
+            }
+            if (square < c) {
+                squares.Add(square);
+                dic[square] = true;
+            }
+            else {
+                break;
+            }
+        }
+        
+        foreach (var sq in squares) {
+            var diff = c - sq;
+            if (dic.ContainsKey(diff)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
