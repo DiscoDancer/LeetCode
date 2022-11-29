b372d4178af88e8bdcec8678015bc9cf93f0316b
discodancer <vlad.rabot9ga@gmail.com>
2022-11-29 13:35:12 +0300

    +1

--- 1000 and 1 tasks/binary-search1/maximum-distance-between-a-pair-of-values.cs
+++ 1000 and 1 tasks/binary-search1/maximum-distance-between-a-pair-of-values.cs
@@ -1,9 +1,13 @@
 public class Solution {
+    // квадрат слишком долго
+    // массивы сортированы, можно ускориться бинарным поиском
+    // ну 2 points навеное тоже ок
     public int MaxDistance(int[] nums1, int[] nums2) {
         var max = 0;
         
+        // можно тут сэкономить, прибавляя max
         for (int i = 0; i < nums1.Length; i++) {
-            for (int j = i + 1; j < nums2.Length; j++) {
+            for (int j = i + 1 + max; j < nums2.Length; j++) {
                 if (nums1[i] <= nums2[j]) {
                     max = Math.Max(max, j - i);
                 }
