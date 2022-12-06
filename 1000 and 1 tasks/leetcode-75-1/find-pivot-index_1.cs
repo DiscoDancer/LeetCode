f6676d068b7c0dc7cd9ee046c776b28725f22624
discodancer <vlad.rabot9ga@gmail.com>
2022-12-06 18:27:01 +0300

    +1

---
+++ 1000 and 1 tasks/leetcode-75-1/find-pivot-index_1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int PivotIndex(int[] nums) {
+        var sum = nums.Sum();
+        var n = nums.Length;
+
+        var leftSum = 0;
+        var rightSum = sum;
+
+        for (int i = 0; i < n; i++) {
+            rightSum -= nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
