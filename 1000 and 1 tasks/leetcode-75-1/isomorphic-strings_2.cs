0d4c5a29273fb37d89b82c823a4ba0a2a8b3336b
discodancer <vlad.rabot9ga@gmail.com>
2022-12-06 19:43:40 +0300

    +1

---
+++ 1000 and 1 tasks/leetcode-75-1/isomorphic-strings_2.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+        if (s.Length != t.Length) {
+            return false;
+        }
+
+        const int AlphabetSize = 128;
+        var currNumber = 200;
+        var dictionary = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++) {
+            if (!dictionary.ContainsKey(s[i])) {
+                dictionary[s[i]] = currNumber++;
+            }
+        }
+
+        var dictionary2 = new Dictionary<char, int>();
+        var currNumber2 = 200;
+        for (int i = 0; i < t.Length; i++) {
+            if (!dictionary2.ContainsKey(t[i])) {
+                dictionary2[t[i]] = currNumber2++;
+            }
+            if (dictionary2[t[i]] != dictionary[s[i]]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
