97c6c9619a3038e837ce718942a6a74de66ac33e
discodancer <vlad.rabot9ga@gmail.com>
2022-12-12 19:25:34 +0300

    +1

---
+++ 1000 and 1 tasks/leetcode-75-1/lowest-common-ancestor-of-a-binary-search-tree_4.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        var cur = root;
+
+        while (true) {
+            if (p.val > cur.val && q.val > cur.val) {
+                cur = cur.right;
+                continue;
+            }
+            if (p.val < cur.val && q.val < cur.val) {
+                cur = cur.left;
+                continue;
+            }
+            else {
+                return cur;
+            }
+        }
+
+        return cur;
+    }
+}
