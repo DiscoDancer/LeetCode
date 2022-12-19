9308eca2b72352092f375c307cb430cec08f4eb6
discodancer <vlad.rabot9ga@gmail.com>
2022-12-19 12:22:19 +0300

    +1

---
+++ 1000 and 1 tasks/leetcode-75-1/decode-string.cs
@@ -0,0 +1,65 @@
+public class Solution {
+
+    // string builder can help
+    // stack мб все только портит
+    public string DecodeString(string s) {
+        var stack = new Stack<string>();
+        for (int i = 0; i < s.Length; i++) {
+            if (s[i] != ']') {
+                stack.Push(s[i].ToString());
+            }
+            else {
+                var buffer = new List<string>();
+                while (stack.Peek() != "[") {
+                    buffer.Add(stack.Pop());
+                }
+                stack.Pop(); // выкинули '['
+                buffer.Reverse();
+                var str = buffer.Aggregate((x,y) => x + y); // срока внутри []
+
+                var numbers = new List<string>();
+                while (stack.Any() && char.IsDigit(stack.Peek()[0])) {
+                    numbers.Add(stack.Pop());
+                }
+                numbers.Reverse();
+                var numStr = numbers.Aggregate((x,y) => x + y);
+                var num = int.Parse(numStr);
+                
+                var newS = "";
+                for (int j = 0; j < num; j++) {
+                    newS += str;
+                }
+
+                stack.Push(newS);
+            }
+        }
+
+        var res = new List<string>();
+
+        while (stack.Any())
+        {
+            res.Add(stack.Pop());
+        }
+
+        res.Reverse();
+
+        return res.Aggregate((x, y) => x + y);
+
+        return stack.Pop();
+    }
+
+    
+
+    // категории: буква, цифра, открыв скобка, закрыв скобка
+    // public string DecodeString(string s) {
+    //     char? prev = null;
+    //     var digits = new List<char>();
+
+    //     for (int i = 0; i < s.Lenght; i++) {
+    //         if (char.IsDigit(s[i])) {
+    //             digits.Add(s[i]);
+    //         }
+    //         prev = s[i];
+    //     }
+    // }
+}
