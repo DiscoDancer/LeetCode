4092d0bcd65e61f2f8f1143551782df24d58a27c
discodancer <vlad.rabot9ga@gmail.com>
2022-12-22 12:02:51 +0300

    +1

---
+++ 1000 and 1 tasks/data-structure1/contains-duplicate.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        var sorted = nums.OrderBy(x => x).ToArray();
+        int prev = sorted[0];
+
+        for (int i = 1; i < sorted.Length; i++) {
+            if (sorted[i] == prev) {
+                return true;
+            }
+
+            prev = sorted[i];
+        }
+
+        return false;
+    }
+}
