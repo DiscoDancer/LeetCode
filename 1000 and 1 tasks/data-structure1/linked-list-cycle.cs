a1e60739e0e38602203f2194e9f59201bd32834f
discodancer <vlad.rabot9ga@gmail.com>
2022-12-23 11:58:39 +0300

    +1

---
+++ 1000 and 1 tasks/data-structure1/linked-list-cycle.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        var slow = head;
+        var fast = head;
+
+        while (fast?.next != null && slow != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
