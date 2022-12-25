6e60e7a7373eb185135caff5406f752361ebb426
discodancer <vlad.rabot9ga@gmail.com>
2022-12-25 18:17:02 +0300

    +1

---
+++ 1000 and 1 tasks/data-structure1/invert-binary-tree_1.cs
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    // уровень + 2 pointers
+    // тоже самое решение, что для симетрик
+    public TreeNode InvertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        var stack = new Stack<TreeNode>();
+        stack.Push(root);
+        
+        while (stack.Any()) {
+            var cur = stack.Pop();
+
+            var tmp = cur.left;
+            cur.left = cur.right;
+            cur.right = tmp;
+
+            if (cur.left != null) {
+                stack.Push(cur.left);
+            }
+
+            if (cur.right != null) {
+                stack.Push(cur.right);
+            }
+        }
+        return root;
+    }
+}
