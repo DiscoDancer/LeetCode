6948948e505b636b028ec99763f7f0501234c349
discodancer <vlad.rabot9ga@gmail.com>
2022-12-26 12:29:52 +0300

    +1

---
+++ 1000 and 1 tasks/data-structure1/insert-into-a-binary-search-tree_1.cs
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+
+    // вопрос - надо его перестраивать или нет
+    // нужно придумать когда надо
+    // на вид не надо его перестраивать
+
+    public TreeNode InsertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        var p = root;
+
+        while (p != null) {
+            if (val > p.val) {
+                if (p.right == null) {
+                    p.right = new TreeNode(val);
+                    return root;
+                } else {
+                    p = p.right;
+                }
+            } else {
+                if (p.left == null) {
+                    p.left = new TreeNode(val);
+                    return root;
+                } else {
+                    p = p.left;
+                }
+            }
+        }
+        
+        return root;
+    }
+}
