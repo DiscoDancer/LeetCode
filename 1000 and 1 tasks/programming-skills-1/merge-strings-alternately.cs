d7ec3ec6a74e3f8da4291d8d6cfef95309ee4a63
discodancer <vlad.rabot9ga@gmail.com>
2022-12-29 11:43:53 +0300

    +1

---
+++ 1000 and 1 tasks/programming-skills-1/merge-strings-alternately.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public string MergeAlternately(string word1, string word2) {
+        var p1 = 0;
+        var p2 = 0;
+        var sb = new StringBuilder();
+
+        while (p1 < word1.Length && p2 < word2.Length) {
+            sb.Append(word1[p1++]);
+            sb.Append(word2[p2++]);
+        }
+
+        while (p1 < word1.Length) {
+            sb.Append(word1[p1++]);
+        }
+
+        while (p2 < word2.Length) {
+            sb.Append(word2[p2++]);
+        }
+
+        return sb.ToString();
+    }
+}
