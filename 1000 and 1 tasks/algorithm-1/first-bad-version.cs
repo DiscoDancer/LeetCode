3d55d99eb45b165fa605f5f1d8e70e0597a17982
discodancer <vlad.rabot9ga@gmail.com>
2022-12-30 00:04:58 +0300

    +1

---
+++ 1000 and 1 tasks/algorithm-1/first-bad-version.cs
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        var a = 1;
+        var b = n;
+
+        while (a <= b) {
+            var m = a + (b-a)/2;
+            if (IsBadVersion(m)) {
+                b = m - 1;
+            }
+            else {
+                a = m + 1;
+            }
+        }
+
+        return b + 1;
+    }
+}
