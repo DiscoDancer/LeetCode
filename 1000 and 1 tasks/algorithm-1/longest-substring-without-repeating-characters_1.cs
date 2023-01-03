95685f7aee82a6809e0ee2aef3843236adeebc3b
discodancer <vlad.rabot9ga@gmail.com>
2023-01-03 23:36:50 +0300

    +1

---
+++ 1000 and 1 tasks/algorithm-1/longest-substring-without-repeating-characters_1.cs
@@ -0,0 +1,46 @@
+public class Solution {
+
+    private bool IsValid(string s) {
+        var table = new HashSet<char>();
+
+        foreach (var c in s) {
+            if (table.Contains(c)) {
+                return false;
+            }
+            table.Add(c);
+        }
+
+        return true;
+    }
+
+
+    /*
+        Roadmap:
+        + самый тупой перебор
+        + оптимизация 1: не смотреть длины больше, если меньшая не проходит
+        + оптимизация 2: не смотреть длины меньше максимальной (уже проходит)
+    */    
+
+    // сначала перебор
+    public int LengthOfLongestSubstring(string s) {
+        if (s.Length == 0) {
+            return 0;
+        }
+
+        var max = 1;
+
+        for (int i = 0; i < s.Length; i++) {
+            for (int l = max; (i + l) <= s.Length ; l++) {
+                var sub = s.Substring(i, l);
+                if (IsValid(sub)) {
+                    max = Math.Max(max, l);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        return max;
+    }
+}
