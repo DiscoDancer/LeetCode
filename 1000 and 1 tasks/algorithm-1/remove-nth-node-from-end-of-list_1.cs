cb115b612795065bcdd22cf2f5830e78785241f7
discodancer <vlad.rabot9ga@gmail.com>
2023-01-03 23:20:51 +0300

    +1

---
+++ 1000 and 1 tasks/algorithm-1/remove-nth-node-from-end-of-list_1.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        var fakeHead = new ListNode();
+        fakeHead.next = head;
+
+        var first = fakeHead;
+        var second = fakeHead;
+
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+
+        return fakeHead.next;
+    }
+}
