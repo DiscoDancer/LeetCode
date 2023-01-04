0737fb9b8cd68b126ee525f5acd46d5ea5598588
discodancer <vlad.rabot9ga@gmail.com>
2023-01-04 21:44:51 +0300

    +1

---
+++ 1000 and 1 tasks/algorithm-1/populating-next-right-pointers-in-each-node.cs
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+*/
+
+public class Solution {
+    // level traversal
+    public Node Connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        var queue = new Queue<Node>();
+        queue.Enqueue(root);
+        var expectedChildren = 1;
+
+        while (queue.Any()) {
+            Node prev = null;
+            var newExpectedChildren = 0;
+            for (int i = 0; i < expectedChildren; i++) {
+                var cur = queue.Dequeue();
+                if (prev != null) {
+                    prev.next = cur;
+                }
+                if (cur.left != null) {
+                    newExpectedChildren++;
+                    queue.Enqueue(cur.left);
+                }
+                if (cur.right != null) {
+                    newExpectedChildren++;
+                    queue.Enqueue(cur.right);
+                }
+                prev = cur;
+            }
+
+            expectedChildren = newExpectedChildren;
+        }
+
+        return root;
+    }
+}
