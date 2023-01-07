5790d23f5c961776f42700880be12e715228b4e1
discodancer <vlad.rabot9ga@gmail.com>
2023-01-07 09:17:50 +0300

    +1

---
+++ 1000 and 1 tasks/dynamic-programming-1/house-robber_1.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int Rob(int[] nums) {
+        if (nums.Length == 1) {
+            return nums[0];
+        }
+
+        var prevPrev = nums[0];
+        var prev = Math.Max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.Length; i++) {
+            var cur = Math.Max(prev, prevPrev + nums[i]);
+            prevPrev = prev;
+            prev = cur;
+        }
+
+        return prev;
+    }
+}
