623b6c3e7ed782aac06d3b089bf2eb4aa1eb3d53
discodancer <vlad.rabot9ga@gmail.com>
2023-01-09 08:45:19 +0300

    +1

--- 1000 and 1 tasks/dynamic-programming-1/maximum-subarray.cs
+++ 1000 and 1 tasks/dynamic-programming-1/maximum-subarray.cs
@@ -1,16 +1,15 @@
 public class Solution {
+    // Kadane algorithm
     public int MaxSubArray(int[] nums) {
         var bestSum = 0;
         var curSum = 0;
-        var hadAnyPositive = false;
+        var max = nums[0];
         foreach (var n in nums) {
             curSum = Math.Max(0, curSum + n);
             bestSum = Math.Max(curSum, bestSum);
-            if (!hadAnyPositive && n >= 0) {
-                hadAnyPositive = true;
-            }
+            max = Math.Max(max, n);
         }
 
-        return hadAnyPositive ? bestSum : nums.Max();
+        return max > 0 ? bestSum : max;
     }
 }
