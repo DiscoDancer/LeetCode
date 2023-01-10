6ddd556066d11fde4f0d6c4edd5afcc70edf3de4
discodancer <vlad.rabot9ga@gmail.com>
2023-01-10 15:53:33 +0300

    +1

--- 1000 and 1 tasks/dynamic-programming-1/best-time-to-buy-and-sell-stock-with-cooldown.cs
+++ 1000 and 1 tasks/dynamic-programming-1/best-time-to-buy-and-sell-stock-with-cooldown.cs
@@ -1,4 +1,5 @@
 public class Solution {
+    // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/submissions/
     public int MaxProfit(int[] prices) {
         var held = int.MinValue;
         var sold = int.MinValue;
