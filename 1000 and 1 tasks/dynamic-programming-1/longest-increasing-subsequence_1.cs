8aadf862fec01a1e291b8779060b6f363e79c0ed
discodancer <vlad.rabot9ga@gmail.com>
2023-01-15 15:40:39 +0300

    +1

--- 1000 and 1 tasks/dynamic-programming-1/longest-increasing-subsequence_1.cs
+++ 1000 and 1 tasks/dynamic-programming-1/longest-increasing-subsequence_1.cs
@@ -1,21 +1,25 @@
 public class Solution {
 
+    // Time limit
     private int LengthOfLISInner(int[] nums, int curIndex, int curMax, int acc) {
-        if (curIndex >= nums.Length ) {
+        if (curIndex >= nums.Length) {
             return acc;
         }
 
-        int include = 0;
-        int exclude = 0;
-
-        if (curMax == -1 || nums[curIndex] > curMax) {
+        // берем текущий
+        int include;
+        if (curMax == -1) {
+            include = LengthOfLISInner(nums, curIndex + 1, nums[curIndex], 1);
+        } else if (nums[curIndex] > curMax) {
             include = LengthOfLISInner(nums, curIndex + 1, nums[curIndex], acc + 1);
+        } else {
+            include = LengthOfLISInner(nums, curIndex + 1, curMax, acc);
         }
 
-        // не берем текущий
-        exclude = LengthOfLISInner(nums, curIndex + 1, curMax, acc);
+        int exclude = LengthOfLISInner(nums, curIndex + 1, curMax, acc);
 
         return Math.Max(include, exclude);
+
     }
 
     public int LengthOfLIS(int[] nums) {
