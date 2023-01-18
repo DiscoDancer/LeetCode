742dfef760f94b58247b9c5e051663e57aad5c7b
discodancer <vlad.rabot9ga@gmail.com>
2023-01-18 11:39:26 +0300

    +1

---
+++ 1000 and 1 tasks/graph-1/number-of-islands.cs
@@ -0,0 +1,60 @@
+public class Solution {
+    private bool[][] Visited;
+    private int IslandCount = 0;
+
+    private void ProcessIsland(char[][] grid, int x0, int y0) {
+        IslandCount++;
+
+        var X = grid.Length;
+        var Y = grid[0].Length;
+
+        var queue = new Queue<(int x, int y)>();
+        queue.Enqueue((x0,y0));
+        Visited[x0][y0] = true;
+
+        while (queue.Any()) {
+            var (x, y) = queue.Dequeue();
+
+            if (x > 0 && grid[x-1][y] == '1' && !Visited[x-1][y]) {
+                queue.Enqueue((x-1,y));
+                Visited[x-1][y] = true;
+            }
+
+            if (x < X - 1 && grid[x+1][y] == '1' && !Visited[x+1][y]) {
+                queue.Enqueue((x+1,y));
+                Visited[x+1][y] = true;
+            }
+
+            if (y > 0 && grid[x][y-1] == '1' && !Visited[x][y-1]) {
+                queue.Enqueue((x,y-1));
+                Visited[x][y-1] = true;
+            }
+
+            if (y < Y - 1 && grid[x][y+1] == '1' && !Visited[x][y+1]) {
+                queue.Enqueue((x,y+1));
+                Visited[x][y+1] = true;
+            }
+        }
+    }
+
+    public int NumIslands(char[][] grid) {
+        var X = grid.Length;
+        var Y = grid[0].Length;
+
+        Visited = new bool[X][];
+        for (int i = 0; i < X; i++) {
+            Visited[i] = new bool[Y];
+        }
+
+        for (int x = 0; x < X; x++) {
+            for (int y = 0; y < Y; y++) {
+                if (grid[x][y] == '0' || Visited[x][y]) {
+                    continue;
+                }
+                ProcessIsland(grid, x, y);
+            }
+        }
+
+        return IslandCount;
+    }
+}
