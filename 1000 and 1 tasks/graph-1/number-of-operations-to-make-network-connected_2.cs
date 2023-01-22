43573611bec39199386da8f111f1ce5a219c0c97
Vladislav Evtushenko <evtushenko.v@outlook.com>
2023-01-22 22:04:41 +0300

    +1

---
+++ 1000 and 1 tasks/graph-1/number-of-operations-to-make-network-connected_2.cs
@@ -0,0 +1,65 @@
+public class Solution {
+    // количество островов можно найти, это будет сколько требуется
+    // вопрос - как найти сколько имеется = найти количество мостиков в группе
+    // вычитаем и профит
+    public int MakeConnected(int n, int[][] connections) {
+        var islands = 0;
+        var spareBridges = 0;
+        var N = n;
+        var visited = new bool[N];
+
+        var startingBridges = new int[N];
+        foreach (var connection in connections) {
+            startingBridges[connection[0]]++;
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            if (visited[i])
+            {
+                continue;
+            }
+
+            var queue = new Queue<int>();
+            queue.Enqueue(i);
+            visited[i] = true;
+
+            var bridgeCount = 0;
+            var nodesCount = 0;
+            
+            while (queue.Any())
+            {
+                var cur = queue.Dequeue();
+                nodesCount++;
+                bridgeCount += startingBridges[cur];
+
+                // здесь тормозим
+                foreach (var connection in connections)
+                {
+                    if (connection[0] == cur && !visited[connection[1]])
+                    {
+                        queue.Enqueue(connection[1]);
+                        visited[connection[1]] = true;
+                    }
+
+                    if (connection[1] == cur && !visited[connection[0]])
+                    {
+                        queue.Enqueue(connection[0]);
+                        visited[connection[0]] = true;
+                    }
+                }
+            }
+
+            spareBridges += Math.Max(0, bridgeCount - (nodesCount - 1));
+
+            islands++;
+        }
+
+        if (spareBridges >= islands - 1)
+        {
+            return islands - 1;
+        }
+
+        return -1;
+    }
+}
