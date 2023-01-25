5f1cc3474252b2e8d656a64655d274e0e1027884
discodancer <vlad.rabot9ga@gmail.com>
2023-01-25 17:57:03 +0300

    +1

---
+++ 1000 and 1 tasks/graph-1/maximal-network-rank_1.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    // n cities: 0..n-1
+    // undirected roads[from,to]
+    public int MaximalNetworkRank(int n, int[][] roads) {
+        var count = new int[n];
+        var connectedWith = new Dictionary<int, HashSet<int>>();
+
+        foreach (var r in roads) {
+            count[r[0]]++;
+            count[r[1]]++;
+
+            if (!connectedWith.ContainsKey(r[0])) {
+                connectedWith[r[0]] = new HashSet<int>();
+            }
+            if (!connectedWith.ContainsKey(r[1])) {
+                connectedWith[r[1]] = new HashSet<int>();
+            }
+            connectedWith[r[0]].Add(r[1]);
+            connectedWith[r[1]].Add(r[0]);
+        }
+
+        var max = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (connectedWith.ContainsKey(i)) {
+                    var minus = connectedWith[i].Contains(j) ? 1 : 0;
+                    max = Math.Max(max, count[i] + count[j] - minus);
+                }
+            }
+        }
+
+        return max;
+    }
+}
