96edd521c6f71fe60a3baae8071ea6b939ff4d93
discodancer <vlad.rabot9ga@gmail.com>
2023-01-31 11:54:11 +0300

    +1

---
+++ 1000 and 1 tasks/explore-graph/dfs/clone-graph_1.cs
@@ -0,0 +1,66 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val, List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+
+// мб можно совсем тупо копирнуть?
+// да, можно сначала таблицу смежности построить
+
+public class Solution {
+    private HashSet<int> _visited = new ();
+    private Dictionary<int, Node> _adjacencyTable = new ();
+
+    // просто DFS обход
+    // добавил заполнение таблицы связности
+    private void StupidDFS(Node node) {
+        if (_visited.Contains(node.val)) {
+            return;
+        }
+
+        if (!_adjacencyTable.ContainsKey(node.val)) {
+            _adjacencyTable[node.val] = new Node(node.val);
+        }
+
+        _visited.Add(node.val);
+
+        foreach (var n in node.neighbors) {
+            if (!_visited.Contains(n.val)) {
+                StupidDFS(n);
+            }
+            if (!_adjacencyTable.ContainsKey(n.val)) {
+                _adjacencyTable[n.val] = new Node(n.val);
+            }
+
+            _adjacencyTable[node.val].neighbors.Add(_adjacencyTable[n.val]);
+        }
+    }
+
+    public Node CloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        StupidDFS(node);
+
+        return _adjacencyTable[node.val];
+    }
+}
