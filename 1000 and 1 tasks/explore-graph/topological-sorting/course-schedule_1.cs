bd3159032a0327afe091c7f9c0014791e4a22ed0
discodancer <vlad.rabot9ga@gmail.com>
2023-02-07 16:46:04 +0300

    +1

---
+++ 1000 and 1 tasks/explore-graph/topological-sorting/course-schedule_1.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    // n cources: 0..n-1
+    // prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+    public bool CanFinish(int numCourses, int[][] prerequisites) {
+       var table = new (List<int> inV, int outVCount)[numCourses];
+       for (int i = 0; i < numCourses; i++) {
+           table[i] = (new List<int>(), 0);
+       }
+
+       foreach (var pr in prerequisites) {
+            var slave = pr[0];
+            var master = pr[1];
+            table[slave] = (table[slave].inV, table[slave].outVCount + 1);
+            table[master].inV.Add(slave);
+       }
+
+       var visited = new bool[numCourses];
+
+       var queue = new Queue<int>();
+       for (int i = 0; i < numCourses; i++) {
+           if (table[i].outVCount == 0) {
+               queue.Enqueue(i);
+           }
+       }
+
+       while (queue.Any()) {
+           var cur = queue.Dequeue();
+           visited[cur] = true;
+
+           foreach (var x in table[cur].inV) {
+               table[x] = (table[x].inV, table[x].outVCount - 1);
+               if (!visited[x] && table[x].outVCount == 0) {
+                   queue.Enqueue(x);
+               }
+           }
+       }
+
+       return visited.All(x => x);
+    }
+}
