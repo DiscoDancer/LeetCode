898c7f51c10ff01f627211d106e0fa227149eec4
discodancer <vlad.rabot9ga@gmail.com>
2023-02-20 12:53:08 +0300

    +1

---
+++ 1000 and 1 tasks/programming-skills-2/reorder-list_2.cs
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ // https://leetcode.com/problems/reorder-list/solutions/577469/reorder-list/?orderBy=most_votes
+public class Solution {
+    // in 1->2->3->4->5->6 find 4
+    private ListNode GetMiddle(ListNode head) {
+        var slow = head;
+        var fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public void ReorderList(ListNode head) {
+        var middle = GetMiddle(head);
+
+        // reverse second part
+        ListNode prev = null;
+        var curr = middle;
+        while (curr != null) {
+            var tmp = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        // merge two sorted linked lists [Problem 21]
+        // merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
+        ListNode first = head, second = prev;
+        while (second.next != null) {
+            var tmp = first.next;
+            first.next = second;
+            first = tmp;
+
+            tmp = second.next;
+            second.next = first;
+            second = tmp;
+        }
+    }
+}
