4d33af6ee9fe732757903706832ab5b1a15209d3
discodancer <vlad.rabot9ga@gmail.com>
2023-02-27 17:47:38 +0300

    +1

---
+++ 1000 and 1 tasks/explore-heap/top-k-frequent-elements.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    // немного копипасты отсюда: // https://leetcode.com/problems/kth-largest-element-in-an-array/editorial/
+    public int[] TopKFrequent(int[] nums, int k) {
+        var table = new Dictionary<int, int>();
+        foreach (var n in nums) {
+            if (!table.ContainsKey(n)) {
+                table[n] = 0;
+            }
+            table[n]++;
+        }
+
+        var queue = new PriorityQueue<int, int>();
+        foreach (var key in table.Keys) {
+            queue.Enqueue(key, table[key]);
+            if (queue.Count > k) {
+                queue.Dequeue();
+            }
+        }
+
+        var result = new int[k];
+        var i = 0;
+        while (queue.TryDequeue(out var key, out var value)) {
+            result[i++] = key;
+        }
+
+        return result;
+    }
+}
