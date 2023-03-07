dcf1e5ddd97f26255ff8bfbfd0f7c4d1c10d54ea
discodancer <vlad.rabot9ga@gmail.com>
2023-03-07 14:48:06 +0300

    +1

---
+++ 1000 and 1 tasks/leetcode-75-2/path-sum-iii.cs
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// https://leetcode.com/problems/path-sum-iii/submissions/
+public class Solution {
+    private int _count = 0;
+    private long _targetSum;
+    private Dictionary<long, int> _dictionary = new ();
+
+    private void TopDown(TreeNode node, long currSum) {
+        if (node == null) {
+            return;
+        }
+
+        currSum += node.val;
+
+        if (currSum == _targetSum) {
+            _count++;
+        }
+
+        if (_dictionary.ContainsKey(currSum - _targetSum)) {
+             _count += _dictionary[currSum - _targetSum];
+        }
+
+        if (_dictionary.ContainsKey(currSum)) {
+            _dictionary[currSum]++;
+        } else {
+            _dictionary[currSum] = 1;
+        }
+
+        TopDown(node.left, currSum);
+        TopDown(node.right, currSum);
+
+        _dictionary[currSum]--;
+    }
+
+    public int PathSum(TreeNode root, int targetSum) {
+        _targetSum = targetSum;
+        TopDown(root, 0L);
+        return _count;
+    }
+}
