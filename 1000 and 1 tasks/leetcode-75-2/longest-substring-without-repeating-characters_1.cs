6ba5df34b8486b7f3dd3d8eb86a18a126de47918
Vladislav Evtushenko <evtushenko.v@outlook.com>
2023-03-15 09:57:18 +0300

    +1

---
+++ 1000 and 1 tasks/leetcode-75-2/longest-substring-without-repeating-characters_1.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        var max = 0;
+        int left = 0;
+        int right = -1;
+        var dic = new Dictionary<char, int>();
+
+        while (right < s.Length - 1) {
+            // пробуем увеличить окно
+            while (right + 1 < s.Length && !dic.ContainsKey(s[right + 1])) {
+                right++;
+                dic[s[right]] = right;
+                max = Math.Max(max, dic.Keys.Count);
+            }
+
+            // пробуем сместиться
+            var isNextWindowValid = false;
+            while (right + 1 < s.Length && !isNextWindowValid) {
+                // удалить левое
+                if (dic[s[left]] == left) {
+                    dic.Remove(s[left]);
+                }
+                left++;
+
+                // добавить или перетереть правое
+                dic[s[right + 1]] = right + 1;
+                right++;
+
+                max = Math.Max(max, dic.Keys.Count);
+                isNextWindowValid = dic.Keys.Count == max;
+            }
+        }
+
+        return max;
+    }
+}
