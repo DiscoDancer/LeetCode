22fcc638c53c46e331c73101d40d16bafa413e01
Владислав Евтушенко <st033188@ad.pu.ru>
2023-04-05 23:32:42 +0200

    +1

---
+++ 1000 and 1 tasks/explore-array-101/height-checker.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int HeightChecker(int[] heights) {
+        var sorted = heights.OrderBy(x => x).ToArray();
+
+        var count = 0;
+        for (int i = 0; i < heights.Length; i++) {
+            if (heights[i] != sorted[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
