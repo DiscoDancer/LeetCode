cec5fdd8aad6571f11913bcb5e6ded949cdcebd1
Владислав Евтушенко <st033188@ad.pu.ru>
2023-04-08 21:50:35 +0200

    +1

---
+++ 1000 and 1 tasks/explore-array-101/max-consecutive-ones-ii_2.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        var result = 0;
+        var curLength = 0;
+        var prevLength = 0;
+        
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == 1) {
+                curLength++;
+            }
+            if (nums[i] == 0 || i == nums.Length - 1) {
+                result = Math.Max(result, curLength + prevLength);
+                prevLength = curLength;
+                curLength = 0;
+            }
+        }
+
+        var any0 = nums.Length != result;
+
+        if (any0) {
+            return result + 1;
+        }
+        return result;
+    }
+}
