4ed6ef04faadc4463fe456f93ad18e7991562a66
Владислав Евтушенко <st033188@ad.pu.ru>
2023-04-10 19:40:50 +0200

    +1

---
+++ 1000 and 1 tasks/data-structure-2/two-sum_1.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var dic = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (dic.ContainsKey(target - nums[i])) {
+                 return new int[] {i, dic[target - nums[i]]};
+            }
+            dic[nums[i]] = i;
+        }
+
+        return null;
+    }
+}
