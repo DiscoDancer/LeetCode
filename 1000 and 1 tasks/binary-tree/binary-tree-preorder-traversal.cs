35bc1a831ffbafc17aca7bb4699abf87497fdf7e
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-05-30 18:53:41 +0200

    +1

---
+++ 1000 and 1 tasks/binary-tree/binary-tree-preorder-traversal.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private List<int> _result = new () {};
+
+    private void F(TreeNode node) {
+        if (node != null) {
+            _result.Add(node.val);
+        }
+        if (node?.left != null) {
+            F(node.left);
+        }
+        if (node?.right != null) {
+            F(node.right);
+        }
+    }
+
+    public IList<int> PreorderTraversal(TreeNode root) {
+        F(root);
+        return _result;
+    }
+}
