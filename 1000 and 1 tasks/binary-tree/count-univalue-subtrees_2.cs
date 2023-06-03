79c878bedd9c653684df1e436da2e5df2dd10e3c
Владислав Евтушенко <st033188@ad.pu.ru>
2023-06-03 21:20:31 +0200

    +1

---
+++ 1000 and 1 tasks/binary-tree/count-univalue-subtrees_2.cs
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ // editorial
+public class Solution {
+    private int _result = 0;
+
+    private bool DFS (TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        var left = DFS(root.left);
+        var right = DFS(root.right);
+
+        if (left && right) {
+            if (root.left != null && root.left.val != root.val) {
+                return false;
+            }
+            if (root.right != null && root.right.val != root.val) {
+                return false;
+            }
+            _result++;
+            return true;
+        }
+
+        return false;
+    } 
+
+    public int CountUnivalSubtrees(TreeNode root) {
+        DFS(root);
+        return _result;
+    }
+}
