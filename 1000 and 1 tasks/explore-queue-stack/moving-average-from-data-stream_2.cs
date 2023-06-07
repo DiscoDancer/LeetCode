01817aff4846ef3ff16eb8a29de7034423e4b2c5
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-06-07 17:18:41 +0200

    +1

---
+++ 1000 and 1 tasks/explore-queue-stack/moving-average-from-data-stream_2.cs
@@ -0,0 +1,49 @@
+public class MovingAverage
+{
+    private readonly int[] _array;
+    private readonly int _size;
+    private int _count = 0;
+    private int _endIndex = -1;
+    private int _beginIndex = -1;
+    private int _currentSum = 0;
+
+    public MovingAverage(int size)
+    {
+        _size = size;
+        _array = new int[size];
+    }
+    
+    public double Next(int val)
+    {
+        if (_count < _size)
+        {
+            _endIndex++;
+            _array[_endIndex] = val;
+            _currentSum += val;
+            _count++;
+            if (_beginIndex == -1)
+            {
+                _beginIndex = _endIndex;
+            }
+        }
+        else
+        {
+            _currentSum -= _array[_beginIndex];
+            _beginIndex++;
+            _beginIndex %= _size;
+
+            _endIndex++;
+            _endIndex %= _size;
+            _array[_endIndex] = val;
+            _currentSum += _array[_endIndex];
+        }
+        
+        return (double)_currentSum / _count;
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.Next(val);
+ */
