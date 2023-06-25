c0ed788d40e53d9aeacccc99985298c93f00857a
Владислав Евтушенко <st033188@ad.pu.ru>
2023-06-25 12:02:35 +0200

    +1

---
+++ 1000 and 1 tasks/explore-hash-table/two-sum-iii-data-structure-design_2.cs
@@ -0,0 +1,40 @@
+public class TwoSum {
+
+    private Dictionary<int, int> _table = new ();
+
+    public TwoSum() {
+        
+    }
+    
+    public void Add(int number) {
+        if (_table.ContainsKey(number)) {
+            _table[number]++;
+        }
+        else {
+            _table[number] = 1;
+        }
+    }
+    
+    public bool Find(int value) {
+        foreach (var k in _table.Keys) {
+            var complement = value - k;
+            if (complement != k) {
+                if (_table.ContainsKey(complement)) {
+                    return true;
+                }
+            }
+            else if (_table[k] > 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.Add(number);
+ * bool param_2 = obj.Find(value);
+ */
