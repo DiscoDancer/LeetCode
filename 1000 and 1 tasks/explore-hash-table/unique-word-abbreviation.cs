c65064af7040ab457b67ab30229192872b971e04
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-06-26 14:20:54 +0200

    +1

---
+++ 1000 and 1 tasks/explore-hash-table/unique-word-abbreviation.cs
@@ -0,0 +1,42 @@
+public class ValidWordAbbr {
+    private string GetWordAbbr(string word) {
+        if (word.Length < 3) {
+            return word;
+        }
+
+        var sb = new StringBuilder();
+        sb.Append(word[0]);
+        sb.Append(word.Length - 2);
+        sb.Append(word[word.Length - 1]);
+
+        return sb.ToString();
+    }
+
+    // can replace list to hashset
+    private Dictionary<string, List<string>> _table = new ();
+
+    public ValidWordAbbr(string[] dictionary) {
+        foreach (var word in dictionary) {
+            var abr = GetWordAbbr(word);
+            if (!_table.ContainsKey(abr)) {
+                _table[abr] = new ();
+            }
+            _table[abr].Add(word);
+        }
+    }
+    
+    public bool IsUnique(string word) {
+        var abr = GetWordAbbr(word);
+        if (!_table.ContainsKey(abr)) {
+            return true;
+        }
+
+        return _table[abr].All(x => x == word);
+    }
+}
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * ValidWordAbbr obj = new ValidWordAbbr(dictionary);
+ * bool param_1 = obj.IsUnique(word);
+ */
