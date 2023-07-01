11dde2a9d97ed66c9c1c8c1b962835506a22868a
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-01 17:14:35 +0200

    +1

---
+++ 1000 and 1 tasks/explore-array-and-string/reverse-string.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        var L = 0;
+        var R = s.Length - 1;
+
+        while (L < R) {
+            var tmp = s[L];
+            s[L] = s[R];
+            s[R] = tmp;
+            L++;
+            R--;
+        }
+    }
+}
