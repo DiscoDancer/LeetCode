5d8ba2c3b3c7606d489958bcfeff74a312b50aa8
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-01 19:06:03 +0200

    +1

---
+++ 1000 and 1 tasks/explore-array-and-string/move-zeroes.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        var j = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == 0) {
+                //
+            }
+            else {
+                nums[j++] = nums[i];
+            }
+        }
+        while (j < nums.Length) {
+            nums[j++] = 0;
+        }
+    }
+}
