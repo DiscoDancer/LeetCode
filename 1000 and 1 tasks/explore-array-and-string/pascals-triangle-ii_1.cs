d46216698d4c7c6597c946da7a4a95e982957bee
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-03 09:31:09 +0200

    +1

---
+++ 1000 and 1 tasks/explore-array-and-string/pascals-triangle-ii_1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public IList<int> GetRow(int rowIndex) {
+        var result = new int[rowIndex + 1];
+        result[0] = 1;
+
+        for (int i = 1; i < rowIndex + 1; i++)
+        {
+            var prev = result[0];
+            for (int j = 1; j < i; j++)
+            {
+                var cur = result[j];
+                var res = cur + prev;
+                prev = cur;
+                result[j] = res;
+            }
+            result[i] = 1;
+        }
+
+        return result;
+    }
+}
