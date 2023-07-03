4e184c6828ca61ceb2b90d17b7f62baeb1cbea2f
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-03 11:58:24 +0200

    +1

---
+++ 1000 and 1 tasks/explore-array-and-string/reverse-words-in-a-string-iii.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public string ReverseWords(string s) {
+        var sb = new StringBuilder();
+        var curWordLength = 0;
+        for (int i = 0; i < s.Length; i++) {
+            if (s[i] != ' ') {
+                curWordLength++;
+            }
+            if ( (s[i] == ' ' || i == s.Length - 1) && curWordLength > 0) {
+                var r = s[i] == ' ' ? i - 1 : i;
+                var l = (r - curWordLength + 1);
+
+                var j = r;
+                while (j >= l) {
+                    sb.Append(s[j]);
+                    j--;
+                }
+                
+                curWordLength = 0;
+            }
+            if (s[i] == ' ') {
+                sb.Append(s[i]);
+            }
+        }
+        return sb.ToString();
+    }
+}
