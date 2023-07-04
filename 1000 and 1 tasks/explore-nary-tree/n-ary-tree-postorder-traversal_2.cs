45de26421c0dc584fa0fd254acc7527b4e8905ff
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-04 09:17:56 +0200

    +1

---
+++ 1000 and 1 tasks/explore-nary-tree/n-ary-tree-postorder-traversal_2.cs
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution {
+    private List<int> _result = new ();
+
+    public IList<int> Postorder(Node root) {
+        var stack = new Stack<Node>();
+        if (root != null) {
+            stack.Push(root);
+        }
+
+        // я интуитивно додумался, что можно сделать наборот с preorder
+        while (stack.Any()) {
+            var cur = stack.Pop();
+            _result.Insert(0, cur.val);
+            foreach (var c in cur.children) {
+                stack.Push(c);
+            }
+        }
+        
+        return _result;
+    }
+}
