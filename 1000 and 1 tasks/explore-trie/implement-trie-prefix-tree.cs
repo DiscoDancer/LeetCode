fb6d3796b1de5626bf702956a94b77615160cece
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-05 11:22:43 +0200

    +1

---
+++ 1000 and 1 tasks/explore-trie/implement-trie-prefix-tree.cs
@@ -0,0 +1,70 @@
+public class Trie {
+
+    public class Node {
+        public char val {get; set;}
+        public Node[] children {get; set;}
+        public bool end { get; set; }
+
+        public Node(char c) {
+            val = c;
+            children = new Node[26];
+        }
+    }
+
+    public Node root = new Node('a');
+
+    public Trie() {
+
+    }
+    
+    public void Insert(string word) {
+        var cur = root;
+        foreach (var c in word) {
+            if (cur.children[c-'a'] == null) {
+                cur.children[c-'a'] = new Node(c);
+            }
+            cur = cur.children[c-'a'];
+        }
+
+        cur.end = true;
+    }
+    
+    public bool Search(string word) {
+        var curNode = root;
+        var i = 0;
+        while (i < word.Length && curNode != null) {
+            var curLetter = word[i];
+            if (curNode.children[curLetter-'a'] == null) {
+                return false;
+            }
+            curNode = curNode.children[curLetter-'a'];
+            i++;
+        }
+
+        return curNode != null && curNode.end;
+    }
+    
+    public bool StartsWith(string prefix) {
+        var curNode = root;
+        for (int i = 0; i < prefix.Length; i++) {
+            if (curNode == null) {
+                return false;
+            }
+            if (curNode.children[prefix[i]-'a'] == null) {
+                return false;
+            }
+            curNode = curNode.children[prefix[i]-'a'];
+        }
+
+        return true;
+    }
+}
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.Insert(word);
+ * bool param_2 = obj.Search(word);
+ * bool param_3 = obj.StartsWith(prefix);
+ */
