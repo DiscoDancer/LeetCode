be6643282d772627b9cb32e05f7473032d241ed4
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-13 16:05:41 +0200

    +1

---
+++ 1000 and 1 tasks/explore-recursion-i/merge-two-sorted-lists_1.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        var fakeHead = new ListNode();
+        var fakeTail = fakeHead;
+
+        while (list1 != null && list2 != null) {
+            var tmp = (ListNode)null;
+            if (list1.val < list2.val) {
+                tmp = list1;
+                list1 = list1.next;
+            }
+            else {
+                tmp = list2;
+                list2 = list2.next;
+            }
+            fakeTail.next = tmp;
+            tmp.next = null;
+            fakeTail = fakeTail.next;
+        }
+
+        fakeTail.next = list1 ?? list2;
+
+        return fakeHead.next;
+    }
+}
