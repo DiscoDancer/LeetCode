180df9064db14749b9831d369b91f4d6d1bd05ce
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-13 15:53:19 +0200

    +1

--- 1000 and 1 tasks/explore-recursion-i/powx-n_3.cs
+++ 1000 and 1 tasks/explore-recursion-i/powx-n_3.cs
@@ -1,17 +1,21 @@
-// stack overflow fixed
+// editorial
 public class Solution {
     public double MyPow2(double x, long n) {
-       if (n == 0) {
-           return 1;
-       }
-       if (n == 1) {
-           return x;
-       }
        if (n < 0) {
            return 1.0 / MyPow2(x, (-1)*n);
        }
 
-       return (n/2 > 0 ? MyPow2(x*x, n/2) : 1.0 ) * (n%2 == 1 ? x : 1.0);
+       var result = 1.0;
+       while (n > 0) {
+           if (n % 2 == 1) {
+               result *= x;
+           }
+
+           x*=x;
+           n/=2;
+       }
+
+       return result;
     }
 
     public double MyPow(double x, int n) {
