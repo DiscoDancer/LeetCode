4f89b58d1c82201e1f4f408fbc13e06b3de09599
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-19 10:23:48 +0200

    +1

---
+++ 1000 and 1 tasks/explore-recursion-ii/combinations3.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    private int _n;
+    private int _k;
+    private IList<IList<int>> _result = new List<IList<int>>(){};
+
+    private void F(List<int> list) {
+        if (list.Count() == _k) {
+            _result.Add(list.ToList());
+            return;
+        }
+
+        var start = list.Any() ? list.Last() : 1;
+        for (int possibleNumber = start; possibleNumber <= _n; possibleNumber++) {
+            if (!list.Contains(possibleNumber)) {
+                list.Add(possibleNumber);
+                F(list);
+                list.Remove(possibleNumber);
+            }
+        }
+    }
+
+    public IList<IList<int>> Combine(int n, int k) {
+        _n = n;
+        _k = k;
+
+        F(new List<int>(){});
+
+        return _result;
+    }
+}
