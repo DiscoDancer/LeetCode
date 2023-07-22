7312f407b71fd929015d46424bd6de9983786cfb
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-22 13:24:39 +0200

    +1

---
+++ 1000 and 1 tasks/explore-recursion-ii/the-skyline-problem_3.cs
@@ -0,0 +1,86 @@
+public class Solution {
+    public IList<IList<int>> GetSkyline(int[][] buildings)
+    {
+        var result = new List<IList<int>>();
+
+        var currentIndexes = new List<int>();
+        var currentHeight = 0;
+        var currentTopIndex = -1;
+
+        var whoStartedTable = new Dictionary<int, List<int>>();
+        var whoEndedTable = new Dictionary<int, List<int>>();
+
+        var list = new List<int>();
+        for (int i = 0; i < buildings.Length; i++)
+        {
+            var b = buildings[i];
+            list.Add(b[1]);
+            list.Add(b[0]);
+            if (!whoStartedTable.ContainsKey(b[0]))
+            {
+                whoStartedTable[b[0]] = new List<int>();
+            }
+            if (!whoEndedTable.ContainsKey(b[1]))
+            {
+                whoEndedTable[b[1]] = new List<int>();
+            }
+            
+            whoStartedTable[b[0]].Add(i);
+            whoEndedTable[b[1]].Add(i);
+        }
+        
+        var arr = list.Distinct().OrderBy(x => x).ToArray();
+        foreach (var x in arr)
+        {
+            var whoStartedIndexes = whoStartedTable.ContainsKey(x) ? whoStartedTable[x] : new List<int>();
+            var whoEndedIndexes = whoEndedTable.ContainsKey(x) ? whoEndedTable[x] : new List<int>();
+            
+            foreach (var i in whoStartedIndexes)
+            {
+                currentIndexes.Add(i);
+                if (buildings[i][2] > currentHeight)
+                {
+                    currentHeight = buildings[i][2];
+                    currentTopIndex = i;
+                }
+            }
+
+            var isTopEnded = false;
+            foreach (var i in whoEndedIndexes)
+            {
+                currentIndexes.Remove(i);
+                if (i == currentTopIndex)
+                {
+                    isTopEnded = true;
+                }
+            }
+
+            if (!result.Any() || currentHeight > result.Last()[1])
+            {
+                var b = buildings[currentTopIndex];
+                result.Add(new List<int>() { b[0], b[2] });
+            }
+            else if (isTopEnded)
+            {
+                var prevHeight = currentHeight;
+                currentHeight = 0;
+                currentTopIndex = -1;
+                foreach (var i in currentIndexes)
+                {
+                    if (buildings[i][2] > currentHeight)
+                    {
+                        currentHeight = buildings[i][2];
+                        currentTopIndex = i;
+                    }
+                }
+
+                if (prevHeight != currentHeight)
+                {
+                    result.Add(new List<int>() { x, currentHeight });
+                }
+            }
+        }
+
+        return result;
+    }
+}
