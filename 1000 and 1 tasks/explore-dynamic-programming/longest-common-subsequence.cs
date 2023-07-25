08b12fccdae23d072e19e4cf3206c41dd23b476a
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-25 13:10:25 +0200

    +1

---
+++ 1000 and 1 tasks/explore-dynamic-programming/longest-common-subsequence.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    private string _text1;
+    private string _text2;
+
+    private int _max = 0;
+
+
+    private void F(int i, int j, int res) {
+        if (i == _text1.Length || j == _text2.Length) {
+            _max = Math.Max(_max, res);
+            return;
+        }
+
+        if (_text1[i] == _text2[j]) {
+            F(i+1,j+1, res + 1);
+        }
+        else {
+            F(i+1, j, res);
+            F(i, j+1, res);
+        }
+    }
+
+    public int LongestCommonSubsequence(string text1, string text2) {
+        _text1 = text1;
+        _text2 = text2;
+
+        F(0,0,0);
+
+        return _max;
+    }
+}
