114973282a1ae34aee7870e4debd9601e36a16d6
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-26 15:06:53 +0200

    +1

---
+++ 1000 and 1 tasks/explore-dynamic-programming/minimum-difficulty-of-a-job-schedule_2.cs
@@ -0,0 +1,54 @@
+public class Solution {
+    private int[] _jobDifficulty;
+    private int _d;
+    private int[,] _mem;
+
+    private int F(int dayIndex, int jobIndex) {
+        if (dayIndex == _d) {
+            return 0;
+        }
+        if (_mem[dayIndex, jobIndex] != 0) {
+            return _mem[dayIndex, jobIndex];
+        }
+
+        var daysRemaining = _d - dayIndex;
+        var jobsRemaining = _jobDifficulty.Length - jobIndex;
+        var availableJobsForDay = jobsRemaining - daysRemaining + 1;
+
+        if (dayIndex == _d -1) {
+            var dayMax = 0;
+            for (int i = 1; i <= availableJobsForDay; i++) {
+                dayMax = Math.Max(dayMax, _jobDifficulty[jobIndex+i-1]);
+            }
+            _mem[dayIndex, jobIndex] = F(dayIndex+1, jobIndex + availableJobsForDay) + dayMax;
+            return  _mem[dayIndex, jobIndex];
+        }
+        else {
+            var dayMax = 0;
+            var minResult = int.MaxValue;
+            for (int i = 1; i <= availableJobsForDay; i++) {
+                dayMax = Math.Max(dayMax, _jobDifficulty[jobIndex+i-1]);
+                var result = F(dayIndex+1, jobIndex+i) + dayMax;
+                minResult = Math.Min(minResult, result);
+            }
+
+            _mem[dayIndex, jobIndex] = minResult;
+
+            return minResult;
+        }
+    }
+
+    // stil TL
+    public int MinDifficulty(int[] jobDifficulty, int d) {
+        if (d > jobDifficulty.Length) {
+            return -1;
+        }
+
+        _mem = new int[d, jobDifficulty.Length];
+
+        _jobDifficulty = jobDifficulty;
+        _d = d;
+
+        return F(0, 0);
+    }
+}
