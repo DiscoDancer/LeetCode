38734db47336e4d1ee449c90c9ac9aa240dd9c6f
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-27 14:05:48 +0200

    +1

---
+++ 1000 and 1 tasks/explore-dynamic-programming/coin-change_2.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    private int _min = int.MaxValue;
+    private int[] _coins;
+
+    private int F(int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+
+        var min = int.MaxValue;
+        foreach (var c in _coins)
+        {
+            if (c == amount) {
+                return 1;
+            }
+            if (c < amount)
+            {
+                var sub = F(amount-c);
+                if (sub != -1) {
+                    min = Math.Min(min, sub);
+                }
+            }
+        }
+        
+        if (min == int.MaxValue) {
+            return -1;
+        }
+        return min + 1;
+    }
+
+    // TL
+    public int CoinChange(int[] coins, int amount) {
+        _coins = coins;
+        return F(amount);
+    }
+}
