04e0c6e7aa3291639f10b204adcacdf81bdbd465
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-28 12:23:38 +0200

    +1

---
+++ 1000 and 1 tasks/explore-dynamic-programming/word-break_1.cs
@@ -0,0 +1,50 @@
+public class Solution {
+    public class Node {
+        public bool IsWord {get; set;} = false;
+        public Dictionary<char, Node> Children {get; set;} = new ();
+    }
+
+    private Node _root = new ();
+    private string _s;
+    private bool _result = false;
+
+    private bool F(int index) {
+        if (index == _s.Length) {
+            return true;
+        }
+
+        var cur = _root;
+        var result = false;
+        for (int i = index; i < _s.Length; i++) {
+            var c = _s[i];
+            if (!cur.Children.ContainsKey(c)) {
+                break;
+            }
+            cur = cur.Children[c];
+            if (cur.IsWord) {
+                result |= F(i+1);
+            }
+        }
+
+        return result;
+    }
+
+    public bool WordBreak(string s, IList<string> wordDict) {
+
+        // buildTree
+        foreach (var w in wordDict) {
+            var cur = _root;
+            foreach (var c in w) {
+                if (!cur.Children.ContainsKey(c)) {
+                    cur.Children[c] = new Node();
+                }
+                cur = cur.Children[c];
+            }
+            cur.IsWord = true;
+        }
+
+        _s = s;
+
+        return F(0);
+    }
+}
