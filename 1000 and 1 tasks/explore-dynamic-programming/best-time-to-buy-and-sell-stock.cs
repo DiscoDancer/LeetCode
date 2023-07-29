66c9874c60a4f5c6408ae4f73a74f86d68163544
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-07-29 12:07:30 +0200

    +1

---
+++ 1000 and 1 tasks/explore-dynamic-programming/best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    private int _maxProfit = 0;
+    private int[] _prices;
+
+    private void F(int dayIndex, int bought) {
+        if (dayIndex == _prices.Length) {
+            return;
+        }
+
+        var todayPrice = _prices[dayIndex];
+        if (bought != -1) {
+            _maxProfit = Math.Max(_maxProfit, todayPrice - bought);
+        }
+
+        F(dayIndex + 1, bought);
+        F(dayIndex + 1, todayPrice);
+    }
+
+    // TL
+    public int MaxProfit(int[] prices) {
+        _prices = prices;
+        F(0, -1);
+
+        return _maxProfit;
+    }
+}
