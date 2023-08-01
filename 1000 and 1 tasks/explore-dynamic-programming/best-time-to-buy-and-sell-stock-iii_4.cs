ba3c41800c3e90981c44430e799fac743d44351c
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-08-01 11:15:06 +0200

    +1

---
+++ 1000 and 1 tasks/explore-dynamic-programming/best-time-to-buy-and-sell-stock-iii_4.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+
+        var table = new int[prices.Length + 1, 2, 4+1];
+        // base кейсы уже прописаны (тк везде 0)
+        // если ли разница куда поставить loop tr ? попал с первого раза
+        for (var dayIndex = prices.Length-1; dayIndex >= 0; dayIndex--) {
+            for (var hold = 0; hold < 2; hold++) {
+                for (var tr = 0; tr < 4; tr++) {
+                    var doNothing = table[dayIndex+1, hold, tr];
+                    if (hold == 0) {
+                        var buy = table[dayIndex+1, 1, tr+1] - prices[dayIndex];
+                        table[dayIndex, hold, tr] = Math.Max(doNothing, buy);
+                    }
+                    else if (hold == 1) {
+                        var sell = table[dayIndex+1, 0, tr +1] + prices[dayIndex];
+                        table[dayIndex, hold, tr] = Math.Max(doNothing, sell);
+                    }
+                }
+            }
+        }
+
+        var max = 0;
+        for (int tr = 0; tr <= 4; tr++) {
+            max = Math.Max(max, table[0,0,tr]);
+        }
+
+        return max;
+    }
+}
