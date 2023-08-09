578667d8ef39f8a1e9a7ab08f3c0c024101c45df
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-08-09 13:12:21 +0200

    +1

---
+++ 1000 and 1 tasks/dynamic-programming-1/maximum-length-of-repeated-subarray.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    private int _max = 0;
+    private int[] _nums1;
+    private int[] _nums2;
+
+    private void F(int index1, int index2, int currentLength) {
+        _max = Math.Max(_max, currentLength);
+
+        if (index1 == _nums1.Length) {
+            return;
+        }
+        if (index2 == _nums2.Length) {
+            return;
+        }
+
+        if (_nums1[index1] == _nums2[index2]) {
+            F(index1 + 1, index2 + 1, currentLength + 1);
+        }
+         {
+            F(index1 + 1, index2, 0);
+            F(index1, index2 + 1, 0);
+        }
+
+        // 1 можем сравнить вот прям сейчас
+        // 2 может отмотать 1
+        // 3 можем отмотать 2
+        // 4 можем отматать оба (это на вид следствие из 2 и 3)
+
+    }
+
+    // TL
+    public int FindLength(int[] nums1, int[] nums2) {
+        _nums1 = nums1;
+        _nums2 = nums2;
+
+        F(0,0,0);
+
+        return _max;
+    }
+}
