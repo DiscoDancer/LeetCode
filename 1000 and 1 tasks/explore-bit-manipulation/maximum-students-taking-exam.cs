0781809ad4da37a34be5eaec7e89a81417339b59
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-08-25 14:51:24 +0200

    +1

---
+++ 1000 and 1 tasks/explore-bit-manipulation/maximum-students-taking-exam.cs
@@ -0,0 +1,73 @@
+public class Solution {
+        // backtracking
+        // state shrinking (ulong)
+
+        private char[][] _seats;
+        private int _max = 0;
+
+        public const char BROKEN = '#';
+        public const char NOT_BROKEN = '.';
+
+        // placed выводится из table
+
+        private void F(int index, bool[,] table) {
+            var X = _seats.Length;
+            var Y = _seats[0].Length;
+
+            var row = index / Y;
+            var col = index % Y;
+
+            bool canPlaced = _seats[row][col] == NOT_BROKEN;
+            if (canPlaced && col > 0 && table[row, col-1]) {
+                canPlaced = false;
+            }
+            // если я иду последовательно, но справа проверять не надо. Точно?
+
+            if (canPlaced && row > 0 && col > 0 && table[row-1, col-1]) {
+                canPlaced = false;
+            }
+            if (canPlaced && row > 0 && col < Y - 1  && table[row-1, col+1]) {
+                canPlaced = false;
+            }
+
+            // last
+            if (index == X*Y -1) {
+                // какой-то грязный фикс, но с ним работает
+                table[row, col] = canPlaced;
+                UpdateMax(table);
+            }
+            else {
+                if (canPlaced) {
+                    table[row, col] = true;
+                    F(index +1, table);
+                    table[row, col] = false;
+                }
+                F(index + 1, table);
+            }
+        }
+
+        private void UpdateMax(bool[,] table) {
+            var count = 0;
+
+            foreach (var a in table) {
+                    if (a) {
+                        count++;
+                    }
+            }
+
+            _max = Math.Max(_max, count);
+        }
+
+        public int MaxStudents(char[][] seats) {
+            _seats = seats;
+
+            var X = seats.Length;
+            var Y = seats[0].Length;
+
+            var table = new bool[X,Y];
+
+            F(0,table);
+
+            return _max;
+        }
+}
