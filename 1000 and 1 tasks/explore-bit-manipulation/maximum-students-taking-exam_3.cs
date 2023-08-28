61494e0ae10c9bd7dc63098b87c88d2dff98e62e
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-08-28 13:50:20 +0200

    +1

---
+++ 1000 and 1 tasks/explore-bit-manipulation/maximum-students-taking-exam_3.cs
@@ -0,0 +1,87 @@
+public class Solution {
+    private char[][] _seats;
+    public const char BROKEN = '#';
+    public const char NOT_BROKEN = '.';
+
+    private bool Read(ulong table, int index)
+    {
+        return ((table >> index) & 1) == 1;
+    }
+
+    private void Write(ref ulong table, int index, bool val)
+    {
+        if (val)
+        {
+            table |= ((ulong)1 << index);
+        }
+        else {
+            table &= (~((ulong)1 << index));
+        }
+    }
+    
+    private int F(int index, ulong table)
+    {
+        var X = _seats.Length;
+        var Y = _seats[0].Length;
+
+        var row = index / Y;
+        var col = index % Y;
+
+        bool canPlaced = _seats[row][col] == NOT_BROKEN;
+        if (canPlaced && col > 0 && Read(table, row * Y + col - 1))
+        {
+            canPlaced = false;
+        }
+        
+        if (canPlaced && row > 0 && col > 0 && Read(table, (row - 1) * Y + col - 1))
+        {
+            canPlaced = false;
+        }
+
+        if (canPlaced && row > 0 && col < Y - 1 && Read(table, (row - 1) * Y + col + 1))
+        {
+            canPlaced = false;
+        }
+        
+        if (index == X * Y - 1)
+        {
+            // какой-то грязный фикс, но с ним работает
+            Write(ref table, row * Y + col, canPlaced);
+            return Calc1(table);
+        }
+
+        if (canPlaced)
+        {
+            Write(ref table, row * Y + col, true);
+            var v1 = F(index + 1, table);
+            Write(ref table, row * Y + col, false);
+
+            return Math.Max(v1, F(index + 1, table));
+        }
+
+        return F(index + 1, table);
+    }
+
+    private int Calc1(ulong table)
+    {
+        var count = 0;
+        while (table > 0)
+        {
+            if (table % 2 == 1)
+            {
+                count++;
+            }
+
+            table /= 2;
+        }
+
+        return count;
+    }
+    
+    public int MaxStudents(char[][] seats)
+    {
+        _seats = seats;
+        
+        return F(0, 0);
+    }
+}
