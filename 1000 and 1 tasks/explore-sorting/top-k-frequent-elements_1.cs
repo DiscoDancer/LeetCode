1d0a3a1b5c0c5e0e52e2b08cf1a0cb6cf98e4b98
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-09-06 13:46:11 +0200

    +1

---
+++ 1000 and 1 tasks/explore-sorting/top-k-frequent-elements_1.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        var ht = new Dictionary<int, int>();
+        foreach (var n in nums) {
+            if (!ht.ContainsKey(n)) {
+                ht[n] = 0;
+            }
+            ht[n]++;
+        }
+
+        var pq = new PriorityQueue<int,int>();
+        foreach (var key in ht.Keys) {
+            pq.Enqueue(key, ht[key]);
+            if (pq.Count > k) {
+                pq.Dequeue();
+            }
+        }
+
+        var result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = pq.Dequeue();
+        }
+
+        return result;
+    }
+}
