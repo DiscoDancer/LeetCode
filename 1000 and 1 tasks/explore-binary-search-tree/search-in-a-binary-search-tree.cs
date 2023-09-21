fa05dd5760a3f33e044c59f4c416da3b5c6837de
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-09-21 14:06:55 +0200

    +1

---
+++ 1000 and 1 tasks/explore-binary-search-tree/search-in-a-binary-search-tree.cs
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        var p = root;
+
+        while (p != null) {
+            if (p.val == val) return p;
+            if (p.val < val) p = p.right;
+            else if (p.val > val) p = p.left;
+        }
+
+        return null;
+    }
+}
