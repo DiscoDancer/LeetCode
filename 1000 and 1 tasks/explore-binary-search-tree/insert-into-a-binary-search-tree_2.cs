12f2d08d19318ff6b50aa05aec4fea637caacd2e
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-09-21 14:34:13 +0200

    +1

--- 1000 and 1 tasks/explore-binary-search-tree/insert-into-a-binary-search-tree_2.cs
+++ 1000 and 1 tasks/explore-binary-search-tree/insert-into-a-binary-search-tree_2.cs
@@ -11,30 +11,40 @@
  *     }
  * }
  */
-public class Solution {
-    public TreeNode InsertIntoBST(TreeNode root, int val) {
-        if (root == null) {
+public class Solution
+{
+    public TreeNode InsertIntoBST(TreeNode root, int val)
+    {
+        if (root == null)
+        {
             return new TreeNode(val);
         }
 
         var p = root;
 
-        while (true) {
-            if (val > p.val) {
-                if (p.right == null) {
+        while (true)
+        {
+            if (val > p.val)
+            {
+                if (p.right == null)
+                {
                     p.right = new TreeNode(val);
                     return root;
                 }
-                else {
+                else
+                {
                     p = p.right;
                 }
             }
-            else if (val < p.val) {
-                if (p.left == null) {
+            else if (val < p.val)
+            {
+                if (p.left == null)
+                {
                     p.left = new TreeNode(val);
                     return root;
                 }
-                else {
+                else
+                {
                     p = p.left;
                 }
             }
