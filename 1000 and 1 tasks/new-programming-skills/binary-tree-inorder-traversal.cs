6651381ec295876149342333975a7d132f9581fe
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-10-02 21:22:38 +0200

    +1

---
+++ 1000 and 1 tasks/new-programming-skills/binary-tree-inorder-traversal.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private List<int> _result = new ();
+
+    private void F(TreeNode root) {
+        if (root.left != null) {
+            F(root.left);
+        }
+
+        _result.Add(root.val);
+
+        if (root.right != null) {
+            F(root.right);
+        }
+    }
+
+    public IList<int> InorderTraversal(TreeNode root) {
+        if (root != null) {
+            F(root);
+        }
+        
+        return _result;
+    }
+}
