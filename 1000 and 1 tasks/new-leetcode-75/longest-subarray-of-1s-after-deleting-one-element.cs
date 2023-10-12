806c3731337587a033b06647747c217c7ec2bae7
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-10-12 14:22:39 +0200

    +1

---
+++ 1000 and 1 tasks/new-leetcode-75/longest-subarray-of-1s-after-deleting-one-element.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    
+    public int LongestOnes(int[] nums, int k) {
+        int left = 0, right;
+        for (right = 0; right < nums.Length; right++) {
+            // If we included a zero in the window we reduce the value of k.
+            // Since k is the maximum zeros allowed in a window.
+            if (nums[right] == 0) {
+                k--;
+            }
+            // A negative k denotes we have consumed all allowed flips and window has
+            // more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
+            if (k < 0) {
+                // If the left element to be thrown out is zero we increase k.
+                k += 1 - nums[left];
+                left++;
+            }
+        }     
+        return right - left;
+    }
+
+    public int LongestSubarray(int[] nums) {
+        return LongestOnes(nums, 1) - 1;
+    }
+}
