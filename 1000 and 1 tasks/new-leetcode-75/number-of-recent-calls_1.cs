2ac9ae0d56b943296182752df91352ea1f761b61
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-10-16 14:22:30 +0200

    +1

---
+++ 1000 and 1 tasks/new-leetcode-75/number-of-recent-calls_1.cs
@@ -0,0 +1,37 @@
+public class RecentCounter {
+
+    private List<int> _calls;
+
+    public RecentCounter() {
+        _calls = new ();
+    }
+    
+    public int Ping(int t) {
+        // t = time now
+
+        _calls.Add(t);
+
+        var timeNow = t;
+        var i = _calls.Count() - 1;
+
+        var result = 0;
+
+        while (i >= 0 && timeNow - _calls[i] <= 3000) {
+            i--;
+            result++;
+        }
+
+        while (i >= 0) {
+            _calls.RemoveAt(i);
+            i--;
+        }
+
+        return result;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.Ping(t);
+ */
