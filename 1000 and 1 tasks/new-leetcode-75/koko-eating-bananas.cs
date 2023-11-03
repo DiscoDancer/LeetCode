06ecc20e1ae4841d0924fd2ef0882c298c8d5ea6
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-11-03 12:51:17 +0100

    +1

---
+++ 1000 and 1 tasks/new-leetcode-75/koko-eating-bananas.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+        var maxSpeed = piles.Max();
+        for (int speed = 1; speed <= maxSpeed; speed++) {
+            var copy = piles.OrderBy(x => x).ToArray();
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < piles.Length; j++) {
+                    if (copy[j] > 0) {
+                        copy[j] -= speed;
+                        break;
+                    }
+                }
+            }
+            if (copy.All(x => x <= 0)) {
+                return speed;
+            }
+        }
+
+        return -1;
+    }
+}
