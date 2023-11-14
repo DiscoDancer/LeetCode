8b66ed1a8ec8cb2c8f6900cd96a5b327006c1b25
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-11-14 14:03:28 +0100

    +1

---
+++ 1000 and 1 tasks/new-leetcode-75/domino-and-tromino-tiling_1.cs
@@ -0,0 +1,50 @@
+public class Solution {
+    private int _n;
+
+    private int Func(int i, bool filledTop, bool filledBottom) {
+        if (i == _n) {
+            if (filledTop && filledBottom) {
+                return 1;
+            }
+            return 0;
+        }
+
+        var result = 0;
+
+        if (filledTop && filledBottom) {
+            // ставлю :
+            result += Func(i+1, true, true);
+            // ничего не ставлю
+            result += Func(i+1, false, false);
+        }
+        else if (!filledTop && !filledBottom) {
+            // ставлю ::
+            result += Func(i+1, true, true);
+            // ставлю :.
+            result += Func(i+1, false, true);
+            // ставлю :'
+            result += Func(i+1, true, false);
+        }
+        else if (filledTop && !filledBottom) {
+            // ставлю .:
+            result += Func(i+1, true, true);
+            // ставлю ..
+            result += Func(i+1, false, true);
+        }
+        else if (!filledTop && filledBottom) {
+            // ставлю ':
+            result += Func(i+1, true, true);
+            // ставлю ..
+            result += Func(i+1, true, false);
+        }
+
+        return result;
+    } 
+
+    // TL
+    public int NumTilings(int n) {
+        _n = n;
+
+        return Func(0, true, true);
+    }
+}
