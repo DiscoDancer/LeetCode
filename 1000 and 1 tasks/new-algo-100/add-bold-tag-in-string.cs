9e2fda3dc17b7e5a2acdd7663eb879c4ef60f090
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-12-14 14:21:45 +0100

    +1

---
+++ 1000 and 1 tasks/new-algo-100/add-bold-tag-in-string.cs
@@ -0,0 +1,65 @@
+public class Solution {
+    // trie
+    // обертка в b легко сделать, у меня будет просто строка s размера в true/false
+    // можно сделать trie и слов, и на каждый символ начинать поиск
+
+    public class Node {
+        public Dictionary<char, Node> Children {get; set;} = new ();
+        public bool IsWord {get; set;} = false;
+    }
+
+    private bool[] _state;
+    private string _s;
+    private Node _root = new Node();
+
+    private void BuildTrie(string[] words) {
+        foreach (var word in words) {
+            var cur = _root;
+            foreach (var c in word) {
+                if (!cur.Children.ContainsKey(c)) {
+                    cur.Children[c] = new ();
+                }
+                cur = cur.Children[c];
+            }
+            cur.IsWord = true;
+        }
+    }
+
+    public string AddBoldTag(string s, string[] words) {
+        _state = new bool[s.Length];
+        _s = s;
+        BuildTrie(words);
+
+        for (int beginIndex = 0; beginIndex < s.Length; beginIndex++) {
+            var cur = _root;
+            var endIndex = beginIndex;
+            while (endIndex < s.Length && cur.Children.ContainsKey(s[endIndex])) {
+                cur = cur.Children[s[endIndex]];
+                if (cur.IsWord) {
+                    var j = beginIndex;
+                    while (j <= endIndex) {
+                        _state[j++] = true;
+                    }
+                }
+                endIndex++;
+            }
+        }
+
+        var sb = new StringBuilder();
+        for (int i = 0; i < _state.Length; i++) {
+            if (!_state[i]) {
+                sb.Append(s[i]);
+            }
+            else {
+                sb.Append("<b>");
+                while (i < _state.Length && _state[i]) {
+                    sb.Append(s[i++]);
+                }
+                i--;
+                sb.Append("</b>");
+            }
+        }
+
+        return sb.ToString();
+    }
+}
