3b65cd63f95668dc75175a88ec8541fcea1f648d
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-12-19 13:13:37 +0100

    +1

--- 1000 and 1 tasks/new-algo-100/first-unique-number.cs
+++ 1000 and 1 tasks/new-algo-100/first-unique-number.cs
@@ -1,37 +1,46 @@
-public class FirstUnique {
+public class FirstUnique
+{
     // хранить уникальные отдельно
     // hash table
     // naive
 
-    private List<int> _listUnique = new ();
-    private Dictionary<int, int> _tableCount = new ();
+    private List<int> _listUnique = new();
+    private Dictionary<int, int> _tableCount = new();
 
-    public FirstUnique(int[] nums) {
-        foreach (var n in nums) {
+    public FirstUnique(int[] nums)
+    {
+        foreach (var n in nums)
+        {
             Add(n);
         }
     }
-    
-    public int ShowFirstUnique() {
-        if (!_listUnique.Any()) {
+
+    public int ShowFirstUnique()
+    {
+        if (!_listUnique.Any())
+        {
             return -1;
         }
         return _listUnique.First();
     }
-    
-    public void Add(int n) {
-            if (!_tableCount.ContainsKey(n)) {
-                _tableCount[n] = 1;
-                _listUnique.Add(n);
-            }
-            else if (_tableCount[n] == 1) {
-                _tableCount[n]++;
-                _listUnique.Remove(n);
-            }
-            else {
-                // можно и ничего не делать
-                _tableCount[n]++;
-            }
+
+    public void Add(int n)
+    {
+        if (!_tableCount.ContainsKey(n))
+        {
+            _tableCount[n] = 1;
+            _listUnique.Add(n);
+        }
+        else if (_tableCount[n] == 1)
+        {
+            _tableCount[n]++;
+            _listUnique.Remove(n);
+        }
+        else
+        {
+            // можно и ничего не делать
+            _tableCount[n]++;
+        }
     }
 }
 
