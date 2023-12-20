af81902ff7f3513c875b61cdc9499fa7a20c6950
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2023-12-20 13:51:29 +0100

    +1

---
+++ 1000 and 1 tasks/new-algo-100/insert-into-a-sorted-circular-linked-list_1.cs
@@ -0,0 +1,84 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+        next = null;
+    }
+
+    public Node(int _val, Node _next) {
+        val = _val;
+        next = _next;
+    }
+}
+*/
+
+// binary search
+// find head, then insert
+
+// пропустить возрастание
+
+public class Solution {
+    public Node Insert(Node head, int insertVal) {
+        if (head == null) {
+            var node = new Node(insertVal);
+            node.next = node;
+            return node;
+        }
+
+        var cur = head;
+        Node prev = null;
+
+        while (cur != cur.next && cur.val <= cur.next.val) {
+            var prevprev = prev;
+            prev = cur;
+            cur = cur.next;
+            // вот это очень важно, чтобы не зациклиться
+            if (cur == head) {
+                cur = prev;
+                prev = prevprev;
+                break;
+            }
+        }
+
+        
+        // если мы здесь, то cur = самый большой
+        // вставляем в конец
+        if (insertVal >= cur.val) {
+            var node = new Node(insertVal);
+            var next = cur.next;
+            cur.next = node;
+            node.next = next;
+            return head;
+        }
+        
+        // теперь cur самый маленький
+        prev = cur;
+        cur = cur.next;
+
+        // вставляем в начало, работает и с [1] + 0
+        if (insertVal <= cur.val) {
+            var node = new Node(insertVal);
+            prev.next = node;
+            node.next = cur;
+            return head;
+        }
+
+        // если мы здесь, то ответ где-то по-середине, и длина списка минимум 2
+        while (!(cur.val <= insertVal && insertVal <= cur.next.val)) {
+            cur = cur.next;
+        }
+        var node2 = new Node(insertVal);
+        var next2 = cur.next;
+        cur.next = node2;
+        node2.next = next2;
+
+
+        return head;
+    }
+}
