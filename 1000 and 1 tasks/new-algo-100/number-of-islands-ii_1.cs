d91fcf7b97e1b9810bd1df9d181558944cd9dd16
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-01-19 14:10:18 +0100

    +1

---
+++ 1000 and 1 tasks/new-algo-100/number-of-islands-ii_1.cs
@@ -0,0 +1,92 @@
+public class Solution {
+    // several aliases for islands
+    // naive: k * m*n (aka check for every update)
+    // optimization 1 : no need to check is there is no water
+
+    /*
+        Ищем соседей. Какие случаи могут быть?
+            - соседеней нет -> новый остров
+            - соседи есть, id острова у всех одинаковый -> старый отсров, берем любой alias
+            - соседи есть, id островов разные -> merge aliases
+    */
+
+    // out of mem
+
+    public class Island {
+        public Island(int alias) {
+            Aliases = new();
+            Aliases.Add(alias);
+        }
+        public Island(List<int> aliases)
+        {
+            Aliases = aliases;
+        }
+        public List<int> Aliases {get; }
+    }
+
+    private int _inslandCount = 0;
+    private Dictionary<(int, int), int> _state = new();
+    private List<Island> _islands = new ();
+    private int _nextAlias = 1;
+
+    private Island FindIsland(int aliase) {
+        return _islands.Single(x => x.Aliases.Contains(aliase));
+    }
+    
+    public IList<int> NumIslands2(int m, int n, int[][] positions) {
+        var result = new List<int>();
+
+        var X = m;
+        var Y = n;
+
+        foreach (var xy in positions) {
+            var x = xy[0];
+            var y = xy[1];
+            if (_state.ContainsKey((x,y)) && _state[(x,y)] != 0) {
+                result.Add(_islands.Count());
+                continue;
+            }
+
+            var neighbours = new List<Island>();
+            if (_state.ContainsKey((x+1,y))) {
+                neighbours.Add(FindIsland(_state[(x+1,y)]));
+            }
+            if (_state.ContainsKey((x-1,y))) {
+                neighbours.Add(FindIsland(_state[(x-1,y)]));
+            }
+            if (_state.ContainsKey((x,y+1))) {
+                neighbours.Add(FindIsland(_state[(x,y+1)]));
+            }
+            if (_state.ContainsKey((x,y-1))) {
+                neighbours.Add(FindIsland(_state[(x,y-1)]));
+            }
+
+            if (neighbours.Count() == 0) {
+                var alias = _nextAlias++;
+                var island = new Island(alias);
+                _islands.Add(island);
+                _state[(x,y)] = alias;
+            }
+            else if (neighbours.Count() == 1) {
+                var island = neighbours.First();
+                var alias = island.Aliases.First();
+                _state[(x,y)] = alias;
+            }
+            else
+            {
+                var aliases = new List<int>();
+                foreach (var neighbour in neighbours)
+                {
+                    aliases.AddRange(neighbour.Aliases);
+                    _islands.Remove(neighbour);
+                }
+                _islands.Add(new Island(aliases));
+                _state[(x,y)] = aliases.First();
+            }
+
+            result.Add(_islands.Count());
+        }
+
+        return result;
+    }
+}
