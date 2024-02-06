544a37579613afe262c978e9d8d8a1c2d743c5ea
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-02-06 12:01:18 +0100

    +1

---
+++ 1000 and 1 tasks/new-algo-100/rearrange-string-k-distance-apart_1.cs
@@ -0,0 +1,75 @@
+// TL
+
+public class Solution {
+    // generate all substrings, filter out good
+
+    // private List<string> _cadidates = new List<string>();
+
+
+    private void Generate(StringBuilder sb, List<char> chars)
+    {
+        if (_result != "")
+        {
+            return;
+        }
+        
+        if (!chars.Any())
+        {
+            var cadidate = sb.ToString();
+            if (IsValid(cadidate))
+            {
+                _result = cadidate;
+            }
+            return;
+        }
+
+        for (int i = 0; i < chars.Count; i++)
+        {
+            var c = chars[i];
+
+            sb.Append(chars[i]);
+            chars.RemoveAt(i);
+
+            Generate(sb, chars);
+
+            chars.Insert(i, c);
+            sb.Remove(sb.Length - 1, 1);
+        }
+    }
+
+    private int _k;
+    private string _result = "";
+
+    private bool IsValid(string cadidate)
+    {
+        var indexes = new int?[26];
+        for (int i = 0; i < cadidate.Length; i++)
+        {
+            var c = cadidate[i];
+
+            if (indexes[c - 'a'].HasValue)
+            {
+                var d = i - indexes[c - 'a'].Value;
+                if (d < _k)
+                {
+                    return false;
+                }
+            }
+
+            indexes[c - 'a'] = i;
+        }
+
+        _result = cadidate;
+        return true;
+    }
+
+    // s consists of only lowercase English letters.
+    public string RearrangeString(string s, int k)
+    {
+        _k = k;
+        var chars = s.ToCharArray().ToList();
+        Generate(new StringBuilder(), chars);
+
+        return _result;
+    }
+}
