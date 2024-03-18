ba9fd3d8712a9497661bea7d6751193c5944fd2b
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-03-18 13:49:01 +0100

    +1

---
+++ 1000 and 1 tasks/new-algo-100/handshakes-that-dont-cross_1.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    // editorial
+    private static int m = 1000000007;
+
+    public int NumberOfWays(int numPeople) {
+        long[] dp = new long[numPeople / 2 + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= numPeople / 2; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += (long) dp[j] * dp[i - j - 1] % m;
+                dp[i] %= m;
+            }
+        }
+        return (int)dp[numPeople / 2]; 
+    }
+}
