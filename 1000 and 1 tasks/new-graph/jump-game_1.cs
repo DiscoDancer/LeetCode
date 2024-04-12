6675acdeb6c4342bce3bc40d45dee6f17d90f0ff
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-04-12 17:47:36 +0200

    +1

---
+++ 1000 and 1 tasks/new-graph/jump-game_1.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    // больше звучит как dp
+
+
+    private int[] _nums;
+    private bool?[] _mem;
+
+    private bool F(int i) {
+        if (i >= _nums.Length ) {
+            return false;
+        }
+        if (i == _nums.Length - 1) {
+            return true;
+        }
+        if (_mem[i] != null) {
+            return _mem[i].Value;
+        }
+
+        var result = false;
+
+        for (int j = 1; j <= _nums[i]; j++) {
+            result = result || F(i+j);
+        }
+
+        _mem[i] = result;
+        return result;
+    }
+
+    public bool CanJump(int[] nums) {
+        _nums = nums;
+        _mem = new bool?[_nums.Length];
+
+        return F(0);
+    }
+}
