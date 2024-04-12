a3d47e8bd62e2b6edae7b3fd28bbf61efa3eb00e
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-04-12 16:59:01 +0200

    +1

---
+++ 1000 and 1 tasks/new-graph/minimum-genetic-mutation_1.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public int MinMutation(string startGene, string endGene, string[] bank) {
+        if (startGene == endGene) {
+            return 0;
+        }
+
+        var queue = new Queue<string>();
+        queue.Enqueue(startGene);
+
+        var copyBank = bank.ToList();
+
+        var generation = 0;
+        while (queue.Any()) {
+            var size = queue.Count();
+
+            for (int i = 0; i < size; i++) {
+                var cur = queue.Dequeue();
+                if (cur == endGene) {
+                    return generation;
+                }
+
+                var optionsToDelete = new List<string>();
+
+                foreach (var option in copyBank) {
+                    var diffCount = 0;
+                    for (int j = 0; j < option.Length && diffCount < 3; j++) {
+                        if (option[j] != cur[j]) {
+                            diffCount++;
+                        }
+                    }
+                    if (diffCount == 1) {
+                        queue.Enqueue(option);
+                        optionsToDelete.Add(option);
+                    }
+                }
+
+                foreach (var x in optionsToDelete) {
+                    copyBank.Remove(x);
+                }
+            }
+            generation++;
+        }
+
+        return -1;
+    }
+}
