102317b608b64d800631bf649f297d43e3b77609
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-06-06 16:01:51 +0200

    +1

---
+++ 1000 and 1 tasks/new-top-150/majority-element_2.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    // editorial
+    public int MajorityElement(int[] nums) {
+        int count = 0;
+        int? candidate = null;
+
+        foreach (int num in nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate.Value;
+    }
+}
