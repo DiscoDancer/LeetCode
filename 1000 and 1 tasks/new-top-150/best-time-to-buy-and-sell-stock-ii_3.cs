0013eefc34868c07588e89b4e7ad08634d0c53ca
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-06-07 14:13:58 +0200

    +1

---
+++ 1000 and 1 tasks/new-top-150/best-time-to-buy-and-sell-stock-ii_3.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public const int YES = 0;
+    public const int NO = 1;
+
+    public int MaxProfit(int[] prices) {
+        var table = new int[prices.Length + 1, 2];
+
+        for (int i = prices.Length - 1; i >= 0; i--) {
+            for (int canBuy = 0; canBuy < 2; canBuy++) {
+                var idle = table[i+1, canBuy];
+                if (canBuy == YES) {
+                    var buy = table[i+1, NO] - prices[i];
+                    table[i, canBuy] = Math.Max(buy, idle);
+                }
+                else {
+                    var sell = table[i+1, YES] + prices[i];
+                    table[i, canBuy] = Math.Max(sell, idle);
+                }
+            }
+        }
+
+        return table[0, 0];
+    }
+}
