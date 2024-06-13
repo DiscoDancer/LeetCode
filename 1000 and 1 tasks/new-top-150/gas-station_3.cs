b787dec7e42f4e372e7bcdca1c9fea1ff352018a
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-06-13 20:01:31 +0200

    +1

---
+++ 1000 and 1 tasks/new-top-150/gas-station_3.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    private int[] _gas;
+    private int[] _cost;
+
+    // ее нужно свести к F(start, end): bool и меморизировать
+    private bool Check(int startIndex) {
+        var n = _gas.Length;
+        var balance = 0;
+        for (int i = 0; i < n; i++) {
+            var trueI = (startIndex + i) % n;
+            balance -= _cost[trueI];
+            balance += _gas[trueI];
+
+            if (balance < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int CanCompleteCircuit(int[] gas, int[] cost) {
+        _gas = gas;
+        _cost = cost;
+
+        for (int startIndex = 0; startIndex < gas.Length; startIndex++) {
+            if (gas[startIndex] >= cost[startIndex] && Check(startIndex)) {
+                return startIndex;
+            }
+        }
+
+        return -1;
+    }
+}
