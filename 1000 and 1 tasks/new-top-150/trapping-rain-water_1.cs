1b565b14dc6b0668fd807f84cf2b3b41ca152388
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-06-19 13:42:53 +0200

    +1

---
+++ 1000 and 1 tasks/new-top-150/trapping-rain-water_1.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public int Trap(int[] height) {
+        var n = height.Length;
+        var sum = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (height[i - 1] > height[i]) {
+                var leftWallIndex = i - 1;
+
+                // вариант 1 правая стена >= левой
+                var rightWallIndex = i;
+                while (rightWallIndex < n && height[rightWallIndex] < height[leftWallIndex]) {
+                    rightWallIndex++;
+                }
+                // вариант 2, самый большой отеносительно текущей воды
+                if (rightWallIndex == n) {
+                    rightWallIndex = i;
+                    var k = rightWallIndex;
+                    while (k < n) {
+                        if (height[k] > height[rightWallIndex]) {
+                            rightWallIndex = k;
+                        }
+                        k++;
+                    }
+                }
+                if (rightWallIndex < n && rightWallIndex != i) {
+                    var j = leftWallIndex + 1;
+                    var subSum = 0;
+                    var min = Math.Min(height[rightWallIndex] , height[leftWallIndex]);
+                    while (j < rightWallIndex) {
+                        subSum += min - height[j];
+                        j++;
+                    }
+                    sum += subSum;
+                    i = rightWallIndex - 1;
+                }
+            }
+        }
+
+        return sum;
+    }
+}
