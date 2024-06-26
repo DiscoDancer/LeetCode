717de59d39a8693aaffc55014a14f86736dbefe5
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-06-26 14:39:45 +0200

    +1

---
+++ 1000 and 1 tasks/new-top-150/zigzag-conversion.cs
@@ -0,0 +1,54 @@
+public class Solution {
+    public string Convert(string s, int numRows) {
+        var table = new char[numRows, s.Length];
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < s.Length; j++) {
+                table[i,j] = '-';
+            }
+        }
+
+        // full cycle = down + diagonal
+        var columnIndex = 0;
+        var cycles = 0;
+        for (int i = 0; i < s.Length; i++) {
+            // down
+            for (var li = 0; li < numRows && i < s.Length; li++) {
+                table[li, columnIndex] = s[i++];
+            }
+            columnIndex++;
+            
+            var lineIndex = numRows - 2; // prev last
+            while (lineIndex > 0 && columnIndex < s.Length && i < s.Length)
+            {
+                table[lineIndex--, columnIndex++] = s[i++];
+            }
+            i--;
+            cycles++;
+            if (cycles == 2)
+            {
+                // break;
+            }
+            // break;
+        }
+
+        var result = new StringBuilder();
+        
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (table[i, j] != '-')
+                    result.Append(table[i, j]);
+                    // Console.Write(table[i,j]);
+                else
+                {
+                    // Console.Write(" ");
+                }
+            }
+
+            // Console.WriteLine();
+        }
+
+        return result.ToString();
+    }
+}
