c9cd88ccb9d6a84e42a0c9003a859487c50b4551
Vladislav.Evtushenko <Vladislav.Evtushenko@entaingroup.com>
2024-07-31 13:41:10 +0200

    +1

---
+++ 1000 and 1 tasks/new-top-150/two-sum-ii-input-array-is-sorted.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        var l = 0;
+        var r = numbers.Length - 1;
+
+        while (l < r) {
+            var sum = numbers[l] + numbers[r];
+            if (sum == target) {
+                return new int[] {l+1, r+1};
+            }
+            else if (sum < target) {
+                l++;
+            }
+            else {
+                r--;
+            }
+        }
+
+        return null;
+    }
+}
